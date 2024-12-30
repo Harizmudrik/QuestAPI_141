dd64063583e779babd8aec437d8e0cfdc94ff024
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-30 14:47:36 +0700

    menambahkan fungsi yang digunakan untuk mengambil data mahasiswa berdasarkan NIM yang diberikan.

--- app/src/main/java/com/example/prak8mysqldatainternet/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/prak8mysqldatainternet/ui/viewmodel/DetailViewModel.kt
@@ -1,10 +1,14 @@
 package com.example.prak8mysqldatainternet.ui.viewmodel
 
 import MahasiswaRepository
+import android.net.http.HttpException
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.prak8mysqldatainternet.model.Mahasiswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.io.IOException
 
 
 sealed class DetailUiState {
@@ -16,3 +20,20 @@ sealed class DetailUiState {
 class DetailViewModel(private val repository: MahasiswaRepository) : ViewModel() {
     private val _uiState = MutableStateFlow<DetailUiState>(DetailUiState.Loading)
     val uiState: StateFlow<DetailUiState> = _uiState
+
+    fun getMahasiswaById(nim: String) {
+        viewModelScope.launch {
+            _uiState.value = DetailUiState.Loading
+            try {
+                val mahasiswa = repository.getMahasiswaById(nim)
+                _uiState.value = DetailUiState.Success(mahasiswa)
+            } catch (e: IOException) {
+                e.printStackTrace()
+                _uiState.value = DetailUiState.Error
+            } catch (e: HttpException) {
+                e.printStackTrace()
+                _uiState.value = DetailUiState.Error
+            }
+        }
+    }
+}
