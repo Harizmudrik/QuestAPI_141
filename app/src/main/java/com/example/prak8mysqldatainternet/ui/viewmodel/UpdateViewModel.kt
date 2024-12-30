4f45557f26e0d53223713b224134962f75f6c623
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-30 14:31:05 +0700

    menambahkan fungsi yang digunakan untuk memuat data mahasiswa berdasarkan NIM yang diberikan.

--- app/src/main/java/com/example/prak8mysqldatainternet/ui/viewmodel/UpdateViewModel.kt
+++ app/src/main/java/com/example/prak8mysqldatainternet/ui/viewmodel/UpdateViewModel.kt
@@ -5,7 +5,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class UpdateViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
     var uiState by mutableStateOf(UpdateUiState())
         private set
+
+    fun loadMahasiswa(nim: String) {
+        viewModelScope.launch {
+            try {
+                val mahasiswa = mhs.getMahasiswaById(nim)
+                uiState = UpdateUiState(updateUiEvent = mahasiswa.toUpdateUiEvent())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
