b17163458f2e34d31ffad1bfd34881ee22479dca
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-30 13:01:39 +0700

    menambahkan fungsi untuk mengubah state uiState berdasarkan input dari UI.

--- app/src/main/java/com/example/prak8mysqldatainternet/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/prak8mysqldatainternet/ui/viewmodel/InsertViewModel.kt
@@ -3,3 +3,8 @@ package com.example.prak8mysqldatainternet.ui.viewmodel
 class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
     var uiState by mutableStateOf(InsertUiState())
         private set
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent) {
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+}
