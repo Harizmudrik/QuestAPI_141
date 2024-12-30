48a0ea76ffd05ce31cf84d9ff884f507b455f2f4
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-30 13:38:03 +0700

    menambahkan deklarasi Fungsi TopAppBar

---
+++ app/src/main/java/com/example/prak8mysqldatainternet/ui/customwidget/TopAppBar.kt
@@ -0,0 +1,16 @@
+package com.example.prak8mysqldatainternet.ui.customwidget
+
+import androidx.compose.material3.TopAppBarScrollBehavior
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CostumeTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {},
+    onRefresh: () -> Unit = {}
+) {
+
+}
