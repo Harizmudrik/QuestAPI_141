5e1dba2681d34c1fd1f32c394ea6bee7dc082101
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-30 13:27:46 +0700

    menambahkan representasi state UI untuk fitur ini, menggunakan sealed class agar state dapat dikelompokkan dan dibatasi.

---
+++ app/src/main/java/com/example/prak8mysqldatainternet/ui/viewmodel/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.prak8mysqldatainternet.ui.viewmodel
+
+sealed class HomeUiState {
+    data class Success(val mahasiswa: List<Mahasiswa>) : HomeUiState()
+    object Error : HomeUiState()
+    object Loading : HomeUiState()
+}
