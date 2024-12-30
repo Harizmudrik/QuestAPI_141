8dd7eb1bd7969aa8b0a25dee80cc101262c7096a
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-30 14:11:19 +0700

    menambahkan import untuk funcition dalam "PenyediaViewModel"

--- app/src/main/java/com/example/prak8mysqldatainternet/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/prak8mysqldatainternet/ui/viewmodel/PenyediaViewModel.kt
@@ -1,5 +1,11 @@
 package com.example.prak8mysqldatainternet.ui.viewmodel
 
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.prak8mysqldatainternet.MahasiswaApplications
+
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
