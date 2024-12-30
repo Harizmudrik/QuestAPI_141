3559a64c086028871efed1a5e7f8e0df0f2feba7
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-30 13:47:55 +0700

    membuat fungsi Utama: "EntryMhsScreen"

--- app/src/main/java/com/example/prak8mysqldatainternet/ui/view/InsertView.kt
+++ app/src/main/java/com/example/prak8mysqldatainternet/ui/view/InsertView.kt
@@ -4,3 +4,13 @@ object DestinasiEntry : DestinasiNavigasi {
     override val route = "item_entry"
     override val titleRes = "Entry Mhs"
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
