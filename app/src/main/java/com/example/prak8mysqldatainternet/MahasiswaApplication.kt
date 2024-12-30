48cfa0472e4ff076cba0b85b73eac2b711ea25f0
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-30 12:57:40 +0700

    menambahkan container dengan instans "MahasiswaContainer" untuk menyediakan dependensi di seluruh aplikasi

--- app/src/main/java/com/example/prak8mysqldatainternet/MahasiswaApplication.kt
+++ app/src/main/java/com/example/prak8mysqldatainternet/MahasiswaApplication.kt
@@ -2,4 +2,9 @@ package com.example.prak8mysqldatainternet
 
 class MahasiswaApplications:Application() {
     lateinit var container: AppContainer
+    override fun onCreate() {
+        super.onCreate()
+        container = MahasiswaContainer()
+    }
+
 }
