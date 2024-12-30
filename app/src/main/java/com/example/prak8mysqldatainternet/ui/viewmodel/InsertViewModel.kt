c90629bab636661d263540cfa8b1942aff978793
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-30 13:04:16 +0700

    menambahkan data class untuk merepresentasikan event input dari UI

--- app/src/main/java/com/example/prak8mysqldatainternet/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/prak8mysqldatainternet/ui/viewmodel/InsertViewModel.kt
@@ -22,4 +22,12 @@ data class InsertUiState(
     val insertUiEvent: InsertUiEvent = InsertUiEvent()
 )
 
+data class InsertUiEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val jenisKelamin: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
 
