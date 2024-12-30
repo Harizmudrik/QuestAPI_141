875c18d961c0614127ff5d91cc792cf528bba937
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-30 12:58:22 +0700

    menerapkan semua import dari "MahasiswaApplication"

--- app/src/main/java/com/example/prak8mysqldatainternet/MahasiswaApplication.kt
+++ app/src/main/java/com/example/prak8mysqldatainternet/MahasiswaApplication.kt
@@ -1,5 +1,10 @@
 package com.example.prak8mysqldatainternet
 
+import android.app.Application
+import com.example.prak8mysqldatainternet.depedenciesinjection.AppContainer
+import com.example.prak8mysqldatainternet.depedenciesinjection.MahasiswaContainer
+
+
 class MahasiswaApplications:Application() {
     lateinit var container: AppContainer
     override fun onCreate() {
