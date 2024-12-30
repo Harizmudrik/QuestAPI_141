820a3cb9d73f54ab5c4a25bd777e52fa8277e675
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-30 14:46:22 +0700

    menambahkan "ViewModel" yang bertanggung jawab untuk mengambil data detail mahasiswa menggunakan "MahasiswaRepository"

--- app/src/main/java/com/example/prak8mysqldatainternet/ui/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/prak8mysqldatainternet/ui/viewmodel/DetailViewModel.kt
@@ -1,6 +1,10 @@
 package com.example.prak8mysqldatainternet.ui.viewmodel
 
+import MahasiswaRepository
+import androidx.lifecycle.ViewModel
 import com.example.prak8mysqldatainternet.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 
 sealed class DetailUiState {
@@ -8,3 +12,7 @@ sealed class DetailUiState {
     data class Success(val mahasiswa: Mahasiswa) : DetailUiState()
     object Error : DetailUiState()
 }
+
+class DetailViewModel(private val repository: MahasiswaRepository) : ViewModel() {
+    private val _uiState = MutableStateFlow<DetailUiState>(DetailUiState.Loading)
+    val uiState: StateFlow<DetailUiState> = _uiState
