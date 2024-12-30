899187c6028b4129a78632d3096c6bdc2a79b9f0
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-30 14:13:44 +0700

    menambahkan fungsi yang bertanggung jawab untuk mengelola navigasi antar halaman dalam aplikasi

---
+++ app/src/main/java/com/example/prak8mysqldatainternet/navigation/PengelolaHalaman.kt
@@ -0,0 +1,40 @@
+package com.example.prak8mysqldatainternet.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.prak8mysqldatainternet.ui.view.DestinasiEntry
+import com.example.prak8mysqldatainternet.ui.view.DestinasiHome
+import com.example.prak8mysqldatainternet.ui.view.EntryMhsScreen
+import com.example.prak8mysqldatainternet.ui.view.HomeScreen
+
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                onDetailClick = { nim ->
+                    // Handle detail click
+                }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route) {
+                    popUpTo(DestinasiHome.route) {
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
