92b864c8eb36b4410bb75d274d5718b56dd700e0
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-12-30 14:00:54 +0700

    menambahkan definisi rute navigasi untuk layar ini, yaitu "home"

---
+++ app/src/main/java/com/example/prak8mysqldatainternet/ui/view/HomeView.kt
@@ -0,0 +1,8 @@
+package com.example.prak8mysqldatainternet.ui.view
+
+import com.example.prak8mysqldatainternet.navigation.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
